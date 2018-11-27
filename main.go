6c9cddd0345608668d687a386c32abf8a581109f
tomiokashougo <tomiokasyogo@gmail.com>
2018-11-28 01:32:57 +0900

    ファイルを分割

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -22,139 +23,65 @@ func GenerateImage(filename string) image.Image {
 	return imageObj
 }
 
-// 二値化した画像のデータを返す関数
-func Binarization(imgObject image.Image) *image.Gray {
-
-	// imageデータをグレースケール化したものを作成
-	rec := imgObject.Bounds()
-
-	binary := image.NewGray(rec)
-
-	// グレーイメージに対して二値化処理
-	for v := rec.Min.Y; v < rec.Max.Y; v++ {
-		for h := rec.Min.X; h < rec.Max.X; h++ {
-			c := color.GrayModel.Convert(imgObject.At(h, v))
-			gray, _ := c.(color.Gray)
-			// しきい値で二値化
-			if gray.Y > 128 {
-				gray.Y = 255
-			} else {
-				gray.Y = 0
-			}
-			binary.Set(h, v, gray)
-		}
-	}
-	return binary
-}
-
-// 二値化ではなくグレースケールで比較
-func NoBinarization(imgObject image.Image) *image.Gray {
-
-	// imageデータをグレースケール化したものを作成
-	rec := imgObject.Bounds()
-
-	binary := image.NewGray(rec)
-
-	// グレーイメージに対して二値化処理
-	for v := rec.Min.Y; v < rec.Max.Y; v++ {
-		for h := rec.Min.X; h < rec.Max.X; h++ {
-			c := color.GrayModel.Convert(imgObject.At(h, v))
-			gray, _ := c.(color.Gray)
-
-			binary.Set(h, v, gray)
-		}
-	}
-	return binary
-}
-
-func GrayDiff(g1 *image.Gray, g2 *image.Gray) *image.Gray {
-	//本来は画像サイズが違うことを考慮しないといけないが今回は同じサイズの画像に限るので(~_~;)
-
-	diffBinary := image.NewGray(g1.Rect)
-	gray := color.Gray{Y: 255}
-
-	for v := g1.Rect.Min.Y; v < g1.Rect.Max.Y; v++ {
-		for h := g1.Rect.Min.X; h < g1.Rect.Max.X; h++ {
-			if g1.GrayAt(h, v) != g2.GrayAt(h, v) {
-				diffBinary.SetGray(h, v, gray)
-			}
-		}
-	}
-	return diffBinary
-}
-
-/*Erosion(縮小)処理をするための関数*/
-func ErosionImage(g *image.Gray) *image.Gray {
-	ResultBinary := image.NewGray(g.Rect)
-	gray := color.Gray{Y: 255}
-
-	for v := g.Rect.Min.Y; v < g.Rect.Max.Y; v++ {
-		for h := g.Rect.Min.X; h < g.Rect.Max.X; h++ {
-			g1 := g.GrayAt(h-1, v).Y == 255
-			g2 := g.GrayAt(h+1, v).Y == 255
-			g3 := g.GrayAt(h, v-1).Y == 255
-			g4 := g.GrayAt(h, v+1).Y == 255
-			g5 := g.GrayAt(h-1, v-1).Y == 255
-			g6 := g.GrayAt(h-1, v+1).Y == 255
-			g7 := g.GrayAt(h+1, v-1).Y == 255
-			g8 := g.GrayAt(h+1, v+1).Y == 255
-
-			if g1 && g2 && g3 && g4 && g5 && g6 && g7 && g8 {
-				ResultBinary.SetGray(h, v, gray)
-			}
-		}
-	}
-	return ResultBinary
-}
-
-func DilationImage(g *image.Gray) *image.Gray {
-	ResultBinary := image.NewGray(g.Rect)
-	gray := color.Gray{Y: 255}
-
-	for v := g.Rect.Min.Y; v < g.Rect.Max.Y; v++ {
-		for h := g.Rect.Min.X; h < g.Rect.Max.X; h++ {
-			if g.GrayAt(h, v).Y == 255 {
-				ResultBinary.SetGray(h-1, v, gray)
-				ResultBinary.SetGray(h+1, v, gray)
-				ResultBinary.SetGray(h, v-1, gray)
-				ResultBinary.SetGray(h, v+1, gray)
-				ResultBinary.SetGray(h-1, v-1, gray)
-				ResultBinary.SetGray(h-1, v+1, gray)
-				ResultBinary.SetGray(h+1, v-1, gray)
-				ResultBinary.SetGray(h+1, v+1, gray)
-			}
-
-		}
-	}
-	return ResultBinary
-}
-
 func GenerateFileter(g *image.Gray) *image.RGBA {
 
 	fileter := image.NewRGBA(g.Rect)
+	_ = ScanImage(g)
 
-	// 全体を100分割する
-	//width := (g.Rect.Max.X - g.Rect.Min.X) / 10
-	//height := (g.Rect.Max.Y - g.Rect.Min.Y) / 10
+	return fileter
+}
 
+//精査された二値データから明るいか(True)、暗いか(True)の情報が入った配列を作成
+func ScanImage(g *image.Gray) [][]bool {
 	var lists [][]bool
+	var t bool
+	var point image.Point
+	// 全体を100分割する
+	width := (g.Rect.Max.X - g.Rect.Min.X) / 10
+	height := (g.Rect.Max.Y - g.Rect.Min.Y) / 10
 
 	for v := 0; v < 10; v++ {
 		var list []bool
-
 		for h := 0; h < 10; h++ {
-			var t = true
-
-			rec := image.Rect{}
-
+			point = image.Point{X: width * h, Y: height * v}
+			t = WatchArea(g, width, height, point)
 			list = append(list, t)
 		}
 		lists = append(lists, list)
 	}
-	return fileter
+	fmt.Println(lists)
+	return lists
 }
 
-func DetectDiff() {}
+//指定された範囲内の明るさが大きければTrueを、そうでなければFalseを返す
+func WatchArea(g *image.Gray, width int, height int, p image.Point) bool {
+
+	x := p.X
+	y := p.Y
+
+	var count = 0
+	gray := color.Gray{Y: 255}
+
+	for v := 0; v < width; v++ {
+		for h := 0; h < height; h++ {
+			if g.GrayAt(x+h, y+v) == gray {
+				count++
+			}
+		}
+	}
+	if count > (width*height)/10 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func DrawBound(img *image.RGBA, rect image.Rectangle) *image.RGBA {
+
+	red := color.RGBA{255, 0, 0, 0}
+	//rectの範囲に枠線を書く
+	return img
+}
 
 // 枠線を描く
 func drawBounds(img *image.RGBA, col color.Color, rect image.Rectangle) {
@@ -198,10 +125,10 @@ func main() {
 
 	diffImage = DilationImage(diffImage)
 
-	for i := 0; i < 7; i++ {
+	for i := 0; i < 5; i++ {
 		diffImage = ErosionImage(diffImage)
 	}
-	for i := 0; i < 7; i++ {
+	for i := 0; i < 5; i++ {
 		diffImage = DilationImage(diffImage)
 	}
 
