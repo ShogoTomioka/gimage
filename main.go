f425642fd1c3c7247d217deff3fc580b38d844c6
Shogo_Tomioka <37259600+ShogoTomioka@users.noreply.github.com>
2018-12-02 01:43:15 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,101 +1,21 @@
 package main
 
 import (
-<<<<<<< HEAD
-=======
 	"flag"
 	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/jpeg"
->>>>>>> master
 	"image/png"
 	"os"
 
-<<<<<<< HEAD
 	"github.com/ShogoTomioka/go-images/lib"
 )
 
 func main() {
-=======
-const (
-	DIVISION  = 10
-	THREAHOLD = 10
-)
 
-func GenerateImage(filename string) image.Image {
-
-	//画像ファイルのオープン
-	file, _ := os.Open(filename)
-	defer file.Close()
-
-	//ファイルをデコードしてImageオブジェクトを作成
-	imageObj, _, err := image.Decode(file)
-	if err != nil {
-		panic(err)
-	}
-	return imageObj
-}
-
-//GenerateFilter は二値画像の明るい部分に枠線を描く
-func GenerateFilter(g *image.Gray) *image.RGBA {
-
-	//間違っている部分を示すためのフィルターImage
-	filter := image.NewRGBA(g.Rect)
-	scanList := ScanImage(g)
-
-	size := g.Rect.Size()
-	width := size.X / DIVISION
 	height := size.Y / DIVISION
-
-	//フィルターの枠線のRectangleを作成、あとでFor分でこのRectangleをずらしていく
-	min := image.Point{X: 0, Y: 0}
-	point := image.Point{X: width, Y: height}
-	rec := image.Rectangle{Min: min, Max: point}
-
-	fmt.Println(rec)
-	for i := 0; i < DIVISION; i++ {
-		for t := 0; t < DIVISION; t++ {
-			if scanList[t][i] == true {
-				p := image.Point{X: width * t, Y: height * i}
-				redRec := rec.Add(p)
-				fmt.Println(redRec)
-				//scanListでTrueになっている部分のRectangleに枠線を描写
-				//	filter.Set(i, t, red)
-				filter = DrawBound(filter, redRec)
-			}
-		}
-	}
-	return filter
-}
-
-//精査された二値データから明るいか(True)、暗いか(True)の情報が入った配列を作成
-func ScanImage(g *image.Gray) [][]bool {
-	var lists [][]bool
-	var t bool
-	var point image.Point
-
-	// 全体を100分割する
-	size := g.Rect.Size()
-	width := size.X / DIVISION
-	height := size.Y / DIVISION
-
-	for v := 0; v < DIVISION; v++ {
-		var list []bool
-		for h := 0; h < DIVISION; h++ {
-			point = image.Point{X: width * h, Y: height * v}
-			t = WatchArea(g, width, height, point)
-			list = append(list, t)
-		}
-		lists = append(lists, list)
-	}
-	return lists
-}
-
-//WatchArea は、指定された範囲内の明るさが大きければTrueを、そうでなければFalseを返す
-func WatchArea(g *image.Gray, width int, height int, p image.Point) bool {
->>>>>>> master
 
 	const (
 		FILE_PATH_A = "./pictures/picture_A.png"
@@ -106,7 +26,6 @@ func WatchArea(g *image.Gray, width int, height int, p image.Point) bool {
 	imageA := lib.GenerateImage(FILE_PATH_A)
 	imageB := lib.GenerateImage(FILE_PATH_B)
 
-<<<<<<< HEAD
 	//二つの比較する画像をそれぞれグレースケール化する
 	grayImage := lib.NoBinarization(imageA)
 	grayImageB := lib.NoBinarization(imageB)
@@ -130,50 +49,16 @@ func WatchArea(g *image.Gray, width int, height int, p image.Point) bool {
 
 	if err := png.Encode(file, diffImage); err != nil {
 		panic(err)
-=======
-	for v := 0; v < width; v++ {
-		for h := 0; h < height; h++ {
-			if g.GrayAt(x+h, y+v) == gray {
-				count++
-			}
-		}
-	}
-	if count > (width*height)/THREAHOLD {
-		return true
-	} else {
-		return false
-	}
-}
-
-//DrawBound は指定されたRectangleの範囲に赤い枠線を描く
-func DrawBound(img *image.RGBA, rect image.Rectangle) *image.RGBA {
-	//間違っている部分を囲う枠線の色
-	red := color.RGBA{255, 0, 0, 0}
-	//rectの範囲に枠線を書く
-	// 上下の枠
-	for h := rect.Min.X; h < rect.Max.X; h++ {
-		img.Set(h, rect.Min.Y, red)
-		img.Set(h, rect.Max.Y-1, red)
-	}
-	// 左右の枠
-	for v := rect.Min.Y; v < rect.Max.Y; v++ {
-		img.Set(rect.Min.X, v, red)
-		img.Set(rect.Max.X-1, v, red)
-
->>>>>>> master
-	}
 
 	filteredImg := lib.OverlaidFilter(imageA, lists)
 
 	filterFile, _ := os.Create("filtered.png")
 	defer filterFile.Close()
 
-<<<<<<< HEAD
 	if err := png.Encode(filterFile, filteredImg); err != nil {
 		panic(err)
 	}
 
-=======
 	imageA := GenerateImage(FILE_PATH_A)
 	imageB := GenerateImage(FILE_PATH_B)
 
@@ -214,5 +99,4 @@ func DrawBound(img *image.RGBA, rect image.Rectangle) *image.RGBA {
 	if err := jpeg.Encode(file, filter, &jpeg.Options{100}); err != nil {
 		panic(err)
 	}
->>>>>>> master
 }
